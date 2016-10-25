02cf00d067c83ffb555759b5a71143f6d127f678
VENHEADs <VENHEADs@yandex.ru>
2016-10-25 16:48:01 +0300

    Update chars.R

--- chars.R
+++ chars.R
@@ -1,6 +1,6 @@
-ggplot(dd, aes(sc[,1],sc[,2], color = cl)) + geom_point() #6
-sc<-pc$scores #5
-pc<-princomp(d) #4
-cl<-as.factor(cl) #3
-cl<-km$cluster #2
-km<-kmeans(d,10,algorithm = "MacQueen",iter.max = 50)  #1
+> ggplot(dd, aes(sc[,1],sc[,2], color = cl),ymi) +ylim(-1*10^7,2.5*10^7)+geom_point()+xlim(-2*10^7,10)# строим классный график с лимитами и цветом
+sc<-pc$scores #5 сохраняем значения
+pc<-princomp(d) #4 делаем мгк
+cl<-as.factor(cl) #3 делаем еее фактором
+cl<-km$cluster #2 переменная для кластерво и лементов
+km<-kmeans(d,10,algorithm = "MacQueen",iter.max = 50)  #1 разбиваем на кластеры
